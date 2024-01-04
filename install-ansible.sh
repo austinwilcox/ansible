4bf9f6847a1db01bba7bb13747d8cf2eb8ff31e4
Austin Wilcox <awilcox@arbinger.com>
2024-01-04 13:54:48 -0700

    add -y to install scripts for ansible.

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
