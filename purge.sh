df71501a612faff3ae2886b385dbd322b656768b
Austin Wilcox <me@theaustinwilcox.com>
2024-01-05 14:51:19 -0700

    add build essentials to the purge script.

--- purge.sh
+++ purge.sh
@@ -11,5 +11,5 @@ rm ~/.ssh/github_rsa.pub
 rm ~/.ssh/github_rsa
 rm -rf ~/.deno
 
-sudo apt purge curl vim git ufw tmux fzf gimp xclip screenkey tldr ripgrep unzip -y
+sudo apt purge curl vim git ufw tmux fzf gimp xclip screenkey tldr ripgrep unzip build-essentials -y
 sudo apt autoremove -y
