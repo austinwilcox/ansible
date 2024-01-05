793d13185bf3a2fec2b8a5b0cf90eec605b2cb24
Austin Wilcox <awilcox@arbinger.com>
2024-01-04 20:26:22 -0700

    add purge script, small changes to others scripts.

---
+++ purge.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+rm ~/.bashrc
+rm ~/.tmux.conf
+rm -rf ~/.dotfiles
+rm ~/.config/alacritty/alacritty.yml
+rm ~/.config/nvim/init.lua
+rm -rf ~/.config/nvim/lua
+rm ~/.config/compton.conf
+rm -rf ~/ZettelkastenV2
+rm ~/.ssh/github_rsa.pub
+rm ~/.ssh/github_rsa
+rm -rf ~/.deno
+
+sudo apt purge curl vim git ufw tmux fzf gimp xclip screenkey tldr ripgrep unzip 
+sudo apt autoremove
